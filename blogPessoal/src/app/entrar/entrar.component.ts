4e309cc3b37117f5296820162804552be53bdf35
Gustavo Moraes <gustavomm.contato@gmail.com>
2021-07-29 19:59:23 -0300

    Atualização do blog pessoal do dia 29/07

--- blogPessoal/src/app/entrar/entrar.component.ts
+++ blogPessoal/src/app/entrar/entrar.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../sevice/auth.service';
+import { UserLogin } from './../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.component.css']
 })
 export class EntrarComponent implements OnInit {
-
-  constructor() { }
+userLogin : UserLogin = new UserLogin;
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  window.scroll(0,0)
   }
 
+  test(){
+    alert(JSON.stringify(this.userLogin))
+  }
+
+  entrar(){
+   this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+     this.userLogin = resp
+   })
+  }
 }
