ba9e01684cd7c776409478c63900ae89384f0aa0
Gustavo Moraes <gustavomm.contato@gmail.com>
2021-08-06 19:20:44 -0300

    Parte da atividade post-get-postagem

--- blogPessoal/src/app/inicio/inicio.component.ts
+++ blogPessoal/src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
+import { Tema } from './../model/Tema';
+import { Postagem } from './../model/Postagem';
 import { Router } from '@angular/router';
 import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
+import { User } from '../model/User';
+
 
 @Component({
   selector: 'app-inicio',
@@ -12,8 +18,20 @@ export class InicioComponent implements OnInit {
   nome = environment.nome
   foto = environment.foto
 
+  postagem: Postagem = new Postagem()
+  listaTemas: Tema[]
+  idTema: number
+  tema: Tema = new Tema()
+
+  listaPostagens: Postagem[]
+
+  user: User = new User()
+  idUser = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -22,6 +40,37 @@ export class InicioComponent implements OnInit {
     alert('Sua seção expirou, faça o login novamente.')
     this.router.navigate(['/entrar'])
   }
+    this.getAllTemas()
+    this.getAllPostagens()
   }
 
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{this.listaTemas = resp})
+  }
+
+findByIdTema(){
+  this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{ this.tema})
+}
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
+      this.listaPostagens = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+
+      alert('Postagem realizada com sucesso')
+
+      this.postagem = new Postagem()
+    })
+  }
 }
